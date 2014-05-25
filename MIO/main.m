2c854ef6794cefddfd3ab0555aa5c7fc90c4dc00
Ali Houshmand <ali.houshmand06@gmail.com>
2014-05-25 10:46:04 -0400

    initial commit

---
+++ MIO/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MIO
+//
+//  Created by Ali Houshmand on 5/25/14.
+//  Copyright (c) 2014 Ali Houshmand. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MIOAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MIOAppDelegate class]));
+    }
+}
