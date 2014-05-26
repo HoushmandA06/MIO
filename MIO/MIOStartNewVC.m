b3d3ead4e7632f37ea151ea054c0afcf7bcd2f3e
Ali Houshmand <ali.houshmand06@gmail.com>
2014-05-26 10:52:00 -0400

    add UISegmentedControl

--- MIO/MIOStartNewVC.m
+++ MIO/MIOStartNewVC.m
@@ -18,12 +18,19 @@
     
     UIButton * moveOut;
     
+    UISegmentedControl * segmentedControl;
+    
     UIBarButtonItem * back;
     
     NSArray * fieldNames;
+    NSArray * keyboardTypes;
     NSMutableArray * fields;
     
+    BOOL moveInIsSelected;
+    BOOL moveOutIsSelected;
     
+    UILabel * moveDateLabel;
+    UIDatePicker * moveDate;
     
 }
 
@@ -56,10 +63,19 @@
     [super viewDidLoad];
     
     ///// ADMIN SECTION       
- //   UILabel * adminTitle = [UILabel alloc] initWithFrame:CGRectMake(<#CGFloat x#>, <#CGFloat y#>, <#CGFloat width#>, <#CGFloat height#>)
-    
+    UILabel * adminTitle = [[UILabel alloc] initWithFrame:CGRectMake(100, 100, 568, 60)];
+    adminTitle.font = [UIFont fontWithName:@"Helvetica" size:35];
+    adminTitle.textAlignment = NSTextAlignmentLeft;
+    adminTitle.textColor = [UIColor colorWithWhite:0.95 alpha:1.0];
+    adminTitle.text = @"Enter Admin Details:";
+    [self.view addSubview:adminTitle];
     
     fieldNames = @[@"Resident", @"Phone", @"Email", @"Property",@"Unit #"];
+ 
+
+    keyboardTypes = @[@(UIKeyboardTypeDefault),@(UIKeyboardTypePhonePad),@(UIKeyboardTypeEmailAddress),@(UIKeyboardTypeNamePhonePad),@(UIKeyboardTypeNumbersAndPunctuation)];
+    
+
     
     fields = [@[]mutableCopy];
     
@@ -67,15 +83,43 @@
     for (NSString * name in fieldNames)
     {
         NSInteger index = [fieldNames indexOfObject:name];
-    
-        
-        UITextField * textField = [[UITextField alloc] initWithFrame:CGRectMake(100,(index * 70)+200,568,60)];
+        UITextField * textField = [[UITextField alloc] initWithFrame:CGRectMake(100,(index * 70)+170,568,60)];
         textField.backgroundColor = [UIColor colorWithWhite:0.90 alpha:1.0];
         textField.layer.cornerRadius = 10;
         textField.leftView = [[UIView alloc] initWithFrame:CGRectMake(0,0,10,30)]; // puts the cursor a set amt right of the textfield
         textField.leftViewMode = UITextFieldViewModeAlways;
         textField.placeholder = name;
         textField.autocorrectionType = FALSE;
+        
+    
+        switch ([fieldNames indexOfObject:name])
+        {
+            case 0:
+                textField.keyboardType = UIKeyboardTypeDefault;
+                break;
+         
+            case 1:
+                textField.keyboardType = UIKeyboardTypePhonePad;
+                break;
+            case 2:
+                textField.keyboardType = UIKeyboardTypeEmailAddress;
+                break;
+            case 3:
+                textField.keyboardType = UIKeyboardTypeDefault;
+                break;
+            case 4:
+                textField.keyboardType = UIKeyboardTypeNumbersAndPunctuation;
+                
+            default:break;
+                
+        }
+        
+//        for (NSString * keyboard in keyboardTypes)
+//        {
+//            NSInteger index = [keyboardTypes indexOfObject:keyboard];
+//            [textField setKeyboardType:keyboardTypes[index]];
+//        }
+        
         textField.autocapitalizationType = UITextAutocapitalizationTypeNone;
         textField.delegate = self;
         [textField.layer setBorderColor: [[UIColor lightGrayColor] CGColor]];
@@ -84,52 +128,91 @@
         [textField resignFirstResponder]; //this is what makes keyboard go away
         
         [fields addObject:textField];
-        
         [self.view addSubview:textField];
-        
     }
-
     
-    
-    
+    [self moveInOutDate];
     
     ///// MOVEIN OR MOVEOUT
     
-    moveIn = [[UIButton alloc] initWithFrame:CGRectMake(SCREEN_WIDTH/2-250,800,150,150)];
-    [moveIn setImage:[UIImage imageNamed:@"movein"] forState:UIControlStateNormal];
-    [moveIn addTarget:self action:@selector(moveIn) forControlEvents:UIControlEventTouchUpInside];
+    moveIn = [[UIButton alloc] initWithFrame:CGRectMake(100,600,150,50)];
+    moveIn.backgroundColor = [UIColor lightGrayColor];
+    moveIn.layer.cornerRadius = 10;
+    [moveIn setTitle:@"Move-In" forState:UIControlStateNormal];
+    [moveIn setTitleColor:[UIColor greenColor] forState:UIControlStateSelected];
+    [moveIn setTitleColor:[UIColor lightGrayColor] forState:UIControlStateNormal];
+    [moveIn addTarget:self action:@selector(selected:) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:moveIn];
     
-    moveOut = [[UIButton alloc] initWithFrame:CGRectMake(SCREEN_WIDTH/2+100,800,150,150)];
-    [moveOut setImage:[UIImage imageNamed:@"moveout"] forState:UIControlStateNormal];
-    [moveOut addTarget:self action:@selector(moveOut) forControlEvents:UIControlEventTouchUpInside];
+    moveOut = [[UIButton alloc] initWithFrame:CGRectMake(SCREEN_WIDTH/2+100,600,150,50)];
+    moveOut.backgroundColor = [UIColor lightGrayColor];
+    moveOut.layer.cornerRadius = 10;
+    [moveOut setTitle:@"Move-Out" forState:UIControlStateNormal];
+    [moveOut setTitleColor:[UIColor greenColor] forState:UIControlStateSelected];
+    [moveOut setTitleColor:[UIColor lightGrayColor] forState:UIControlStateNormal];
+    [moveOut addTarget:self action:@selector(selected:) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:moveOut];
+    
+    segmentedControl = [[UISegmentedControl alloc] initWithItems:[NSArray arrayWithObjects:@"Move-In", @"Move-Out", nil]];
+    segmentedControl.frame = CGRectMake(100, 700, 250, 50);
+    segmentedControl.selectedSegmentIndex = 0;
+    segmentedControl.tintColor = [UIColor blackColor];
+    [segmentedControl addTarget:self action:@selector(valueChanged:) forControlEvents: UIControlEventValueChanged];
+    [self.view addSubview:segmentedControl];
 
  }
 
-
-
-
-
-
-
-
-
--(void)moveIn
+- (void)valueChanged:(UISegmentedControl *)segment
 {
     
-    NSLog(@"moveIn selected");
+    if(segment.selectedSegmentIndex == 0) {
+        moveDateLabel.text = @"Move-In Date";
+        
+    } else if(segment.selectedSegmentIndex == 1){
+        moveDateLabel.text = @"Move-Out Date";
+        
+    }
+    
     
 }
 
+-(void)moveInOutDate
+{
+    moveDateLabel = [[UILabel alloc] initWithFrame:CGRectMake(500, 130, 568, 60)];
+    moveDateLabel.font = [UIFont fontWithName:@"Helvetica" size:20];
+    moveDateLabel.textAlignment = NSTextAlignmentLeft;
+    moveDateLabel.textColor = [UIColor colorWithWhite:0.95 alpha:1.0];
+    [self.view addSubview:moveDateLabel];
 
--(void)moveOut
+    ///// DATE PICKER
+    moveDate = [[UIDatePicker alloc] initWithFrame:CGRectMake(SCREEN_WIDTH/2, 570, 240, 60)];
+    moveDate.datePickerMode = UIDatePickerModeDate;
+    moveDate.backgroundColor = [UIColor clearColor];
+    moveDate.layer.borderColor = [UIColor lightGrayColor].CGColor;
+    moveDate.layer.borderWidth = 2;
+    moveDate.layer.cornerRadius = 10;
+    
+    [self.view addSubview:moveDate];
+}
+
+
+-(void)selected:(UIButton *)sender
 {
     
-    NSLog(@"moveOut selected");
-
+    [sender setSelected:!sender.selected];
     
+    
+    if (moveIn.selected)
+    {   moveDateLabel.text = @"Move-In Date";
+        moveOut.selected = NO;
+    } else if (moveOut.selected)
+    {   moveDateLabel.text = @"Move-Out Date";
+        moveIn.selected = NO;
+    }
+
 }
+
+
 
 - (void)didReceiveMemoryWarning
 {
