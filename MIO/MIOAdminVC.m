e372f26a93b6e012aa98c82c0ca730197008da0c
Ali Houshmand <ali.houshmand06@gmail.com>
2014-06-16 13:32:32 -0400

    phone number auto formats in admin

--- MIO/MIOAdminVC.m
+++ MIO/MIOAdminVC.m
@@ -120,6 +120,9 @@
          
             case 1:
                 textField.keyboardType = UIKeyboardTypePhonePad;
+                
+                textField.tag = 99;
+                
                 textField.frame = CGRectMake(100,(1 * 70)+320,261,60);
 
                 
@@ -396,6 +399,58 @@
 
     return YES;
 }
+
+
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string
+{
+    
+
+    if (textField.tag == 99)
+    
+    {
+    NSString *newString = [textField.text stringByReplacingCharactersInRange:range withString:string];
+    NSArray *components = [newString componentsSeparatedByCharactersInSet:[[NSCharacterSet decimalDigitCharacterSet] invertedSet]];
+    NSString *decimalString = [components componentsJoinedByString:@""];
+    
+    NSUInteger length = decimalString.length;
+    BOOL hasLeadingOne = length > 0 && [decimalString characterAtIndex:0] == '1';
+    
+    if (length == 0 || (length > 10 && !hasLeadingOne) || (length > 11)) {
+        textField.text = decimalString;
+        return NO;
+    }
+    
+    NSUInteger index = 0;
+    NSMutableString *formattedString = [NSMutableString string];
+    
+    if (hasLeadingOne) {
+        [formattedString appendString:@"1 "];
+        index += 1;
+    }
+    
+    if (length - index > 3) {
+        NSString *areaCode = [decimalString substringWithRange:NSMakeRange(index, 3)];
+        [formattedString appendFormat:@"(%@) ",areaCode];
+        index += 3;
+    }
+    
+    if (length - index > 3) {
+        NSString *prefix = [decimalString substringWithRange:NSMakeRange(index, 3)];
+        [formattedString appendFormat:@"%@-",prefix];
+        index += 3;
+    }
+    
+    NSString *remainder = [decimalString substringFromIndex:index];
+    [formattedString appendString:remainder];
+    
+    textField.text = formattedString;
+    
+    return NO;
+    }
+    
+    return YES;
+}
+
 
 
 @end
